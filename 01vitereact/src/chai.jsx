c5eeb12974942b3dedcb3314f6f425ae58252f4d
harshita872005 <149318695+harshita872005@users.noreply.github.com>
2025-08-16 17:22:32 +0530

    basic 1 done

--- 01vitereact/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai() {
-  return(
-      <h2>chai is ready now</h2>
-    );
-}
-
-export default Chai;
--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -1,9 +1,17 @@
+import UserCard from "./components/UserCard"
+import './App.css'
+import lovepic from './assets/lovepic.jpeg'
+import maharanapratap from './assets/maharana.webp'
+import prithviraj from './assets/prithviraj.jpg'
 
 function App() {
+
   return (
-    <>
-      <p>Hello ji, I am starting the react Basic</p>
-    </>
+    <div className = "container" >
+    <UserCard name="Love rana" desc = "desc1" image={lovepic} style={{"border-radius":"10px"}}/>
+    <UserCard name="PrithviRaj Chauhan" desc="desc2" image={prithviraj} style={{"border-radius":"10px"}}/>
+    <UserCard name = "Maharana Pratap" desc="desc3" image={maharanapratap} style={{"border-radius":"10px"}}/>
+    </div>
   )
 }
 
